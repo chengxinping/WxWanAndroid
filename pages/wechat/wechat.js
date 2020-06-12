7ffc2ff104872a1dfb9eefe03cfb3165325c4516
chengxinping <972649353@qq.com>
2020-06-12 10:17:33 +0800

    完成公众号模块

--- pages/wechat/wechat.js
+++ pages/wechat/wechat.js
@@ -1,18 +1,134 @@
 // pages/wechat/wechat.js
+
+
+let api = require("../../utils/api.js")
+
+
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-
+		tabCur: 0,
+		wechatTree: [],
+		swiperList: [],
+		isRefresh: false,
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
+		this.getWechatTree()
+	},
 
+	//获取微信公众分类列表
+	getWechatTree: function () {
+
+		api.wechatTree().then(list => {
+			this.setData({
+				wechatTree: list
+			});
+			list.forEach((item, index) => {
+				this.data.swiperList[index] = {
+					datas: [],
+					curPage: 0,
+				}
+			})
+			this.setData({
+				swiperList: this.data.swiperList
+			})
+			let cid = list[0].id
+			api.articleList(0, {
+				cid: cid
+			}).then(articleBody => {
+				this.saveSwiperList(0, articleBody)
+			})
+		})
+	},
+
+	tabChange: function (event) {
+		let index = event.detail.index
+		let id = this.data.wechatTree[index].id
+		this.setData({
+			tabCur: index
+		})
+
+		if (this.data.swiperList[index].datas.length > 0) {
+			return
+		}
+
+
+		this.getArticleList(0, id)
+
+	},
+
+	swiperChange: function (event) {
+		let index = event.detail.current
+		let id = this.data.wechatTree[index].id
+		this.setData({
+			tabCur: index
+		})
+
+
+		if (this.data.swiperList[index].datas.length > 0) {
+			return
+		}
+
+		this.getArticleList(0, id)
+	},
+
+	getArticleList: function (page, id) {
+		api.articleList(page, {
+			cid: id
+		}).then(data => {
+			this.saveSwiperList(this.data.tabCur, data)
+		})
+	},
+
+	saveSwiperList: function (index, data) {
+		this.data.swiperList[index].datas.push(...data.datas)
+		this.data.swiperList[index].curPage = data.curPage
+		this.data.swiperList[index].pageCount = data.pageCount
+		let key = 'swiperList[' + index + ']';
+		this.setData({
+			[key]: this.data.swiperList[index],
+			isRefresh: false
+		})
+	},
+
+	//列表下拉刷新
+	refresh: function () {
+		if (this._freshing) return
+		this._freshing = true
+		let index = this.data.tabCur
+		let id = this.data.wechatTree[index].id
+		this.getArticleList(0, id)
+		this._freshing = false
+	},
+
+	//列表下拉加载更多
+	loadMore: function () {
+		let index = this.data.tabCur
+		let id = this.data.wechatTree[index].id
+		console.log(this.data.swiperList[index])
+		if (this.data.swiperList[index].curPage < this.data.swiperList[index].pageCount) {
+			this.getArticleList(this.data.swiperList[index].curPage, id)
+		} else {
+			wx.showToast({
+				title: '没有更多数据啦~',
+			})
+		}
+	},
+
+	//去文章详情页
+	toArticleDetail: function (event) {
+		let url = encodeURIComponent(event.currentTarget.dataset.link)
+		let title = event.currentTarget.dataset.title
+		wx.navigateTo({
+			url: `../webview/webview?url=${url}&title=${title}`,
+		})
 	},
 
 	/**
--- utils/api.js
+++ utils/api.js
@@ -6,8 +6,15 @@ let topArticles = () => wxRequest.get("/article/top/json")
 
 let homeArticles = (page) => wxRequest.get(`/article/list/${page}/json`)
 
+let wechatTree = () => wxRequest.get("/wxarticle/chapters/json")
+
+// 某个分类下的文章
+let articleList = (page, param) => wxRequest.get(`/article/list/${page}/json`, param)
+
 module.exports = {
     banner: banner,
     topArticles: topArticles,
-    homeArticles: homeArticles
+    homeArticles: homeArticles,
+    wechatTree: wechatTree,
+    articleList: articleList,
 }
